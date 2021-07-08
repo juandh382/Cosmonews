5b6e7757ebd623ffd7c9c055c35a3dd2dd8553ef
Juan Diego <juandh382@gmail.com>
2021-07-07 20:50:43 -0400

    Nuevos estilos agregados

--- wp-content/themes/cosmonews/functions.php
+++ wp-content/themes/cosmonews/functions.php
@@ -2,7 +2,7 @@
 
 function estilos() {
     wp_enqueue_style('estilos-padre', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('estilos-personalizados', get_stylesheet_directory_uri() . '/css/estilos-personalizados.css', array(), '1.0');
+    wp_enqueue_style('estilos-personalizados', get_stylesheet_directory_uri() . '/css/estilos-personalizados.css', array(), '1.1');
 }
 
 
